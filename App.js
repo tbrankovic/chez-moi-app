b52a18e51f7cf2524850de1d06360de207a67c66
tbrankov <tbrankovic@juniorisep.com>
2021-02-28 09:03:59 +0100

    Crée la navigation

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Tabs from './navigation/tabs';
+import { Home, Offers, Reservations, Account } from './screens';
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    return (
+        <NavigationContainer>
+            <Stack.Navigator screenOptions={{ headerShown: false }}
+                             initialRouteName = { "Home" }
+            >
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+                <Stack.Screen name="Home" component={ Tabs } />
+                <Stack.Screen name="Offers" component={ Offers } />
+                <Stack.Screen name="Reservations" component={ Reservations } />
+                <Stack.Screen name="Account" component={ Account } />
+
+
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
---
+++ screens/Account.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+const Account = () => {
+    return (
+        <View>
+            <Text>Compte</Text>
+        </View>
+    )
+}
+
+export default Account;
