524abbfec73ac364916409a40a8e351719c8802b
tbrankov <tbrankovic@juniorisep.com>
2021-02-28 10:49:02 +0100

    Mise à jour de la navigation

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 
 import Tabs from './navigation/tabs';
+import { COLORS, icons } from './constants'
 import { Home, Offers, Reservations, Account } from './screens';
 
 const Stack = createStackNavigator();
@@ -12,10 +13,9 @@ const Stack = createStackNavigator();
 export default function App() {
     return (
         <NavigationContainer>
-            <Stack.Navigator screenOptions={{ headerShown: false }}
+            <Stack.Navigator screenOptions={{ headerShown: false, cardStyle: { backgroundColor: COLORS.dark2 } }}
                              initialRouteName = { "Home" }
             >
-
                 <Stack.Screen name="Home" component={ Tabs } />
                 <Stack.Screen name="Offers" component={ Offers } />
                 <Stack.Screen name="Reservations" component={ Reservations } />
