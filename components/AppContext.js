7b79157e268e51864b5c030ab4e214287ba3ae6f
tbrankov <tbrankovic@juniorisep.com>
2021-04-04 20:35:34 +0200

    Création de contexte

--- components/AppContext.js
+++ components/AppContext.js
@@ -1,3 +1,3 @@
-import react from 'react'
+import React from 'react'
 const AppContext = React.createContext()
 export default AppContext
--- components/headers/HomeHeader.js
+++ components/headers/HomeHeader.js
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { Text, View, TouchableOpacity, Dimensions, StyleSheet, Image } from 'react-native';
 import appTheme, {FONTS} from "../../constants/theme";
 import images from "../../constants/images";
+import AppContext from "../AppContext";
 
-export class HomeHeader extends React.Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text style={appTheme.FONTS.largeTitle}>Accueil</Text>
+const HomeHeader = () => {
+    const context = useContext(AppContext);
+
+    return (
+        <View style={styles.container}>
+            <Text style={appTheme.FONTS.largeTitle}>Accueil</Text>
+            {context.loggedIn ?
                 <TouchableOpacity style={styles.redRounded}>
                     <Text style={appTheme.FONTS.body3}>Connexion/inscription</Text>
+                </TouchableOpacity> :
+                <TouchableOpacity style={styles.greyRounded}>
+                    <Text style={appTheme.FONTS.body3}>10 jetons</Text>
                 </TouchableOpacity>
-            </View>
-        )
-    }
+            }
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
@@ -27,6 +33,17 @@ const styles = StyleSheet.create({
     redRounded: {
         backgroundColor: appTheme.COLORS.red1,
         padding: 8,
+        paddingLeft: 10,
+        paddingRight: 10,
+        borderRadius: 99
+    },
+    greyRounded: {
+        backgroundColor: appTheme.COLORS.dark1,
+        padding: 8,
+        paddingLeft: 10,
+        paddingRight: 10,
         borderRadius: 99
     }
 })
+
+export default HomeHeader
